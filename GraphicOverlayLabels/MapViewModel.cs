0325fabffed583083d87815f68f5418608f826cd
Viktor Safar <viktor.safar@geodata.no>
2025-04-01 08:43:27 +0200

    Redo as layer

--- GraphicOverlayLabels/MapViewModel.cs
+++ GraphicOverlayLabels/MapViewModel.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Mapping.Labeling;
@@ -13,6 +15,8 @@ namespace GraphicOverlayLabels
     /// </summary>
     public class MapViewModel : INotifyPropertyChanged
     {
+        private const string FieldName = "Name";
+
         public MapViewModel()
         {
             _map = new Map(SpatialReferences.WebMercator)
@@ -20,31 +24,56 @@ namespace GraphicOverlayLabels
                 InitialViewpoint = new Viewpoint(
                     new Envelope(-180, -85, 180, 85, SpatialReferences.Wgs84)
                 ),
-                // Basemap = new Basemap(BasemapStyle.ArcGISStreets)
+                Basemap = new Basemap(BasemapStyle.ArcGISStreets)
             };
 
+            string expressionScript = $"$feature." + FieldName;
+            var expression = new ArcadeLabelExpression(expressionScript);
+
             var labelDef = new LabelDefinition(
-                new SimpleLabelExpression("Some text"),
+                expression,
                 new TextSymbol()
                 {
-                    Color = System.Drawing.Color.Red,
-                    Size = 40,
+                    Color = System.Drawing.Color.Black,
+                    Size = 10,
                     HaloColor = System.Drawing.Color.White,
                     HaloWidth = 2
                 }
             );
 
-            var go = new GraphicsOverlay() { LabelsEnabled = true, Id = "test labels" };
-            go.LabelDefinitions.Add(labelDef);
-            go.Renderer = new SimpleRenderer(
+            var g = new Graphic() { Geometry = new MapPoint(0, 0, SpatialReferences.Wgs84) };
+            g.Attributes.Add(FieldName, "Homer");
+
+            var table = new FeatureCollectionTable([g], GetFields());
+            var collectionLayer = new FeatureCollectionLayer(new FeatureCollection([table]))
+            {
+                Id = "Test collection",
+            };
+            var layer = collectionLayer.Layers[0];
+            layer.Id = "Testlayer";
+            layer.Name = "Test layer";
+            layer.Renderer = GetRenderer();
+            layer.LabelsEnabled = true;
+            layer.LabelDefinitions.Add(labelDef);
+            Map.OperationalLayers.Add(collectionLayer);
+
+            //var go = new GraphicsOverlay() { LabelsEnabled = true, Id = "test labels" };
+            //go.LabelDefinitions.Add(labelDef);
+            //go.Renderer = GetRenderer();
+            //go.Graphics.Add(g);
+            //GraphicsOverlays.Add(go);
+        }
+
+        private Renderer GetRenderer()
+        {
+            return new SimpleRenderer(
                 new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.Red, 20)
             );
+        }
 
-            var g = new Graphic() { Geometry = new MapPoint(0, 0) };
-            g.Attributes.Add("name", "Homer");
-            go.Graphics.Add(g);
-
-            _graphicsOverlays.Add(go);
+        private List<Field> GetFields()
+        {
+            return [new Field(FieldType.Text, FieldName, null, 20)];
         }
 
         private Map _map;
